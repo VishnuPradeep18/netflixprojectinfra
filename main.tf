bdf246459985cfebb25d69d9e5726ac08e66e5f6
VishnuPradeep18 <113281667+VishnuPradeep18@users.noreply.github.com>
2025-03-15 19:28:18 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+region = "us-east-1"
+}
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-04aa00acb1165b32a"
+instance_type = "t2.medium"
+key_name = "todaykeypair"
+vpc_security_groups_ids = ["sg-099b5dfe1e93b463e"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat", "tomcat-2"]
+}
